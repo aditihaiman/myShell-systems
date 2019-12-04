836659786dfca0e1780b4fef57d27cb4a6b602ba
aditihaiman <ahaiman00@stuy.edu>
2019-12-04 15:33:03 -0500

    finished exec/built in commands

--- shell.c
+++ shell.c
@@ -5,21 +5,44 @@
 #include <sys/wait.h>
 
 
-
-
-
-
-
+char ** parse_args( char * line ) {
+    char ** args = malloc(5 * sizeof(char *));
+    char *s1 = line;
+    int idx = 0;
+    char *s;
+    while(line != NULL){
+        args[idx] = strsep( &line, " ");
+        idx++;
+    }
+    args[idx] = NULL;
+    return args;
+}
 
 int main() {
     int x = 1;
     
     while(x) {
-        char * s;
+        char s[100];
         printf("$ ");
         fgets(s, 100, stdin);
-        if (strcmp(s,"exit\n")==0) x = 0;
+        char * new = s;
+        char * command = strsep(&new, "\n");
+        
+        if (strcmp(command,"exit")==0) {
+            x = 0;
+        }
+        
+        int child = fork();
 
+        if (child == 0) {
+             char ** args = parse_args(command);
+             execvp(args[0], args);
+        }
+
+        else {
+            wait(NULL);
+
+        }
         //printf("s:%s cmp: %d\n", s, strcmp(s, "exit\n"));
         
         //sleep(0.5);
