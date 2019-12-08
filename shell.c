ca7876c98da658d8363b19c979209ad90a46ebef
aditihaiman <ahaiman00@stuy.edu>
2019-12-08 10:00:47 -0500

    wrote multiple commands on one line

--- shell.c
+++ shell.c
@@ -25,7 +25,7 @@ char ** parse_mult_args( char * line ) {
     int idx = 0;
     char *s;
     while(line != NULL){
-        args[idx] = strsep( &line, ";");
+        args[idx] = strsep( &line, " ; ");
         idx++;
     }
     args[idx] = NULL;
@@ -41,35 +41,40 @@ int main() {
         fgets(s, 100, stdin);
         char * new = s;
         char * command = strsep(&new, "\n"); //Removes \n from user input
-        char ** args = parse_args(command); //creates list with command to execute
-
-        if (strcmp(args[0],"exit")==0) { //exit
-            printf("[Process completed]\n");
-            break;
-        }
-        else if (strcmp(args[0], "cd")==0){ //change directory
-            //printf("path: %s\n", args[1]);
-            chdir(args[1]);
-            //printf("errno: %d  error: %s\n", errno, strerror(errno));
-        }
-        
-        else {
-            int child = fork();
-
-            if (child == 0) { //creates child process to execute command
-                execvp(args[0], args);
+        char ** mult_args = parse_mult_args(command);
+        int idx = 0;
+        while (mult_args[idx] != NULL) { //loops through each separate command 
+            char ** args = parse_args(mult_args[idx]); //creates list with command to execute
+            //printf("arg0: %s\n", args[0]);
+            if (strcmp(args[0],"exit")==0) { //exit
+                printf("[Process completed]\n");
+                goto end;
             }
-
+            else if (strcmp(args[0], "cd")==0){ //change directory
+                //printf("path: %s\n", args[1]);
+                chdir(args[1]);
+                //printf("errno: %d  error: %s\n", errno, strerror(errno));
+            }
+            
             else {
-                wait(NULL);
+                int child = fork();
 
+                if (child == 0) { //creates child process to execute command
+                    execvp(args[0], args);
+                }
+
+                else {
+                    wait(NULL);
+
+                }
             }
+            idx++;
+            //printf("s:%s cmp: %d\n", s, strcmp(s, "exit\n"));
+            
+            //sleep(0.5);
         }
-        //printf("s:%s cmp: %d\n", s, strcmp(s, "exit\n"));
-        
-        //sleep(0.5);
     }
     
-    
+    end:
     return 0;
 }
